34224fbed78f894076c7cb7daff2195cc3f5c485
Tony Green <tony.green@spacelabs.com>
2019-01-02 10:13:29 -0800

    Initial revision

---
+++ ICSAuditLogDataViewer/ICSAuditLogDataViewer/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ICSAuditLogDataViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.patientIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.applicationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deviceNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.messageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.originalValueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.newValueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.changedByDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.auditLogDataBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.portalDataSet2 = new ICSAuditLogDataViewer.portalDataSet2();
+            this.auditLogDataTableAdapter1 = new ICSAuditLogDataViewer.portalDataSet2TableAdapters.AuditLogDataTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auditLogDataBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portalDataSet2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dateTimeDataGridViewTextBoxColumn,
+            this.patientIDDataGridViewTextBoxColumn,
+            this.applicationDataGridViewTextBoxColumn,
+            this.deviceNameDataGridViewTextBoxColumn,
+            this.messageDataGridViewTextBoxColumn,
+            this.itemNameDataGridViewTextBoxColumn,
+            this.originalValueDataGridViewTextBoxColumn,
+            this.newValueDataGridViewTextBoxColumn,
+            this.changedByDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.auditLogDataBindingSource1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1739, 909);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dateTimeDataGridViewTextBoxColumn
+            // 
+            this.dateTimeDataGridViewTextBoxColumn.DataPropertyName = "DateTime";
+            dataGridViewCellStyle1.Format = "G";
+            dataGridViewCellStyle1.NullValue = null;
+            this.dateTimeDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dateTimeDataGridViewTextBoxColumn.HeaderText = "DateTime";
+            this.dateTimeDataGridViewTextBoxColumn.Name = "dateTimeDataGridViewTextBoxColumn";
+            this.dateTimeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dateTimeDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // patientIDDataGridViewTextBoxColumn
+            // 
+            this.patientIDDataGridViewTextBoxColumn.DataPropertyName = "PatientID";
+            this.patientIDDataGridViewTextBoxColumn.HeaderText = "PatientID";
+            this.patientIDDataGridViewTextBoxColumn.Name = "patientIDDataGridViewTextBoxColumn";
+            this.patientIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.patientIDDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // applicationDataGridViewTextBoxColumn
+            // 
+            this.applicationDataGridViewTextBoxColumn.DataPropertyName = "Application";
+            this.applicationDataGridViewTextBoxColumn.HeaderText = "Application";
+            this.applicationDataGridViewTextBoxColumn.Name = "applicationDataGridViewTextBoxColumn";
+            this.applicationDataGridViewTextBoxColumn.ReadOnly = true;
+            this.applicationDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // deviceNameDataGridViewTextBoxColumn
+            // 
+            this.deviceNameDataGridViewTextBoxColumn.DataPropertyName = "DeviceName";
+            this.deviceNameDataGridViewTextBoxColumn.HeaderText = "DeviceName";
+            this.deviceNameDataGridViewTextBoxColumn.Name = "deviceNameDataGridViewTextBoxColumn";
+            this.deviceNameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.deviceNameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // messageDataGridViewTextBoxColumn
+            // 
+            this.messageDataGridViewTextBoxColumn.DataPropertyName = "Message";
+            this.messageDataGridViewTextBoxColumn.HeaderText = "Message";
+            this.messageDataGridViewTextBoxColumn.Name = "messageDataGridViewTextBoxColumn";
+            this.messageDataGridViewTextBoxColumn.ReadOnly = true;
+            this.messageDataGridViewTextBoxColumn.Width = 500;
+            // 
+            // itemNameDataGridViewTextBoxColumn
+            // 
+            this.itemNameDataGridViewTextBoxColumn.DataPropertyName = "ItemName";
+            this.itemNameDataGridViewTextBoxColumn.HeaderText = "ItemName";
+            this.itemNameDataGridViewTextBoxColumn.Name = "itemNameDataGridViewTextBoxColumn";
+            this.itemNameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.itemNameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // originalValueDataGridViewTextBoxColumn
+            // 
+            this.originalValueDataGridViewTextBoxColumn.DataPropertyName = "OriginalValue";
+            this.originalValueDataGridViewTextBoxColumn.HeaderText = "OriginalValue";
+            this.originalValueDataGridViewTextBoxColumn.Name = "originalValueDataGridViewTextBoxColumn";
+            this.originalValueDataGridViewTextBoxColumn.ReadOnly = true;
+            this.originalValueDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // newValueDataGridViewTextBoxColumn
+            // 
+            this.newValueDataGridViewTextBoxColumn.DataPropertyName = "NewValue";
+            this.newValueDataGridViewTextBoxColumn.HeaderText = "NewValue";
+            this.newValueDataGridViewTextBoxColumn.Name = "newValueDataGridViewTextBoxColumn";
+            this.newValueDataGridViewTextBoxColumn.ReadOnly = true;
+            this.newValueDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // changedByDataGridViewTextBoxColumn
+            // 
+            this.changedByDataGridViewTextBoxColumn.DataPropertyName = "ChangedBy";
+            this.changedByDataGridViewTextBoxColumn.HeaderText = "ChangedBy";
+            this.changedByDataGridViewTextBoxColumn.Name = "changedByDataGridViewTextBoxColumn";
+            this.changedByDataGridViewTextBoxColumn.ReadOnly = true;
+            this.changedByDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // auditLogDataBindingSource1
+            // 
+            this.auditLogDataBindingSource1.DataMember = "AuditLogData";
+            this.auditLogDataBindingSource1.DataSource = this.portalDataSet2;
+            // 
+            // portalDataSet2
+            // 
+            this.portalDataSet2.DataSetName = "portalDataSet2";
+            this.portalDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // auditLogDataTableAdapter1
+            // 
+            this.auditLogDataTableAdapter1.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1739, 909);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "ICS Audit Log Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.auditLogDataBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portalDataSet2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private portalDataSet2 portalDataSet2;
+        private System.Windows.Forms.BindingSource auditLogDataBindingSource1;
+        private portalDataSet2TableAdapters.AuditLogDataTableAdapter auditLogDataTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateTimeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn patientIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn applicationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deviceNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn messageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn originalValueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn newValueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn changedByDataGridViewTextBoxColumn;
+    }
+}
+
