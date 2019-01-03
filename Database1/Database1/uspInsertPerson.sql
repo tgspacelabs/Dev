9057afa27571ca303eca9a5b7bc995c286a42f14
Tony Green <tony.green@spacelabs.com>
2019-01-02 16:06:25 -0800

    Added DB tests.

---
+++ Database1/Database1/uspInsertPerson.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[uspInsertPerson]
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Person]([FirstName], [LastName])
+    VALUES (@FirstName, @LastName);
+
+--SELECT [p].[PersonID], [p].[FirstName], [p].[LastName], [p].[Created], [p].[Updated] 
+--    FROM [dbo].[Person] AS [p];
+
+    RETURN 0
+END
